7023d5c414ac30ac53b67276a6d3d98b18ba6260
samyoshon <samuelyshon@gmail.com>
2015-12-08 10:33:07 -0800

    comment code

--- js/question.js
+++ js/question.js
@@ -1,8 +1,12 @@
+//Constructor function for questions
+
 function Question(text, choices, answer) {
     this.text = text;
     this.choices = choices;
     this.answer = answer;
 }
+
+//Method for questions
 
 Question.prototype.isCorrectAnswer = function (choice) {
     return this.answer === choice;
--- js/quiz.js
+++ js/quiz.js
@@ -1,8 +1,14 @@
+//Constructor function for quiz
+
 function Quiz(questions) {
     this.score = 0;
     this.questions = questions;
     this.currentQuestionIndex = 0;
 }
+
+//Methods for quiz
+
+	//Method 'guess' adds to score and moves questions along via index
 
 Quiz.prototype.guess = function(answer) {
     if(this.getCurrentQuestion().isCorrectAnswer(answer)) {
@@ -11,9 +17,13 @@ Quiz.prototype.guess = function(answer) {
     this.currentQuestionIndex++;
 };
 
+	//Method 'getCurrentQuestion' retrieves next question from question array
+
 Quiz.prototype.getCurrentQuestion = function() {
     return this.questions[this.currentQuestionIndex];
 };
+
+	//Method 'hasEnded' ends game is question index is greater than question array
 
 Quiz.prototype.hasEnded = function() {
     return this.currentQuestionIndex >= this.questions.length;
