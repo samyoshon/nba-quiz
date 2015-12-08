5e1396651f2f674f403610fad0c7972378337c24
samyoshon <samuelyshon@gmail.com>
2015-12-08 11:59:19 -0800

    add NBA related questions

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 //Create Questions
 var questions = [
-    new Question("Who was the first President of the United States?", [ "George Washington", "Thomas Jefferson" ], "George Washington"),
-    new Question("What is the answer to the Ultimate Question of Life, the Universe, and Everything?", ["Pi","42"], "42"),
-    new Question("What is the answer to the Ultimate Question of Life, the Universe, and Everything?", ["Pi","42", "Monkeys"], "42")
+    new Question("Who was drafted earliest in the 1996 NBA draft? ", [ "Antoine Walker", "Kobe Bryant", "Steve Nash", "Ray Allen" ], "Ray Allen"),
+	new Question("Who was the NBA All-Star Game MVP in 2004", [ "Allen Iverson", "Shaquille O'neal", "Kobe Bryant", "Tim Duncan" ], "Shaquille O'neal"),
+	new Question("Who was the leader in Assists Per Game in the 08-09 season?", [ "Allen Iverson", "Steven Nash", "Deron Williams", "Chris Paul" ], "Chris Paul"),
+	new Question("Who was the Rookie of the Year in the 11-12 season?", [ "Blake Griffin", "Derrick Rose", "John Wall", "Landry Fields" ], "Blake Griffin")
 ];
 
 //Create Quiz
